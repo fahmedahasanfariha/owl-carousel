90e7024c54f4ae771e2ec4836422a890272cb799
Fahmeda Hasan Fariha <58398404+fahmedahasanfariha@users.noreply.github.com>
2020-12-09 01:35:57 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:3,
+        margin: 20,
+        loop: true,
+        autoplay:true,
+        autoplayTimeout:1000
+    });
+});
